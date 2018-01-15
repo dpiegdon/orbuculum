9bdb20c003aedc44f364125e93ebc876e9722c88
David R. Piegdon <dgit@piegdon.de>
2018-01-15 03:33:35 +0000

    testbench cleanup: properly reset circuit

--- orbtrace/src/testbench.v
+++ orbtrace/src/testbench.v
@@ -20,7 +20,7 @@ module testbench();
 	reg        pll_clk48 = 1'b0;
 	reg        pll_locked = 1'b0;
 	reg        clk12     = 1'b0;
-	reg        rst       = 1'b1;
+	reg        rst       = 1'b0;
 	wire       D6;
 	wire       D5;
 	wire       D4;
@@ -67,21 +67,9 @@ module testbench();
 		pll_clk48 <= ~pll_clk48;
 
 		clk12 <= ~clk12;
-
-		#10.416;
-		pll_clk48 <= ~pll_clk48;
-		#10.416;
-		pll_clk48 <= ~pll_clk48;
-
-		#10.416;
-		pll_clk48 <= ~pll_clk48;
-		#10.416;
-		pll_clk48 <= ~pll_clk48;
-
-		clk12 <= ~clk12;
 	end
 
-	task setTrace;
+	task setTracePortStimulus;
 		input [3:0] data;
 		begin
 			traceDin <= data;
@@ -92,133 +80,86 @@ module testbench();
 		end
 	endtask
 
+	task setTrace1;
+		parameter dataWidth = 16;
+		input [dataWidth-1:0] data;
+		integer i;
+		begin
+			for(i = dataWidth-1; i >= 0; i=i-1) begin
+				setTracePortStimulus({ 1'b0, 1'b0, 1'b0, data[i] });
+			end
+		end
+	endtask
+
+	task setTrace2;
+		parameter dataWidth = 16;
+		input [dataWidth-1:0] data;
+		integer i;
+		begin
+			for(i = dataWidth-1; i >= 0; i=i-1) begin
+				setTracePortStimulus({ 1'b0, 1'b0, data[i+1], data[i] });
+			end
+		end
+	endtask
+
+	task setTrace4;
+		parameter dataWidth = 16;
+		input [dataWidth-1:0] data;
+		integer i;
+		begin
+			for(i = dataWidth-1; i >= 0; i=i-1) begin
+				setTracePortStimulus({ data[i+3], data[i+2], data[i+1], data[i] });
+			end
+		end
+	endtask
+
 	initial begin
-		traceClk <= 1'b0;
 		traceDin <= 4'b0000;
+		traceClk <= 1'b0;
+		#1000;
+
+		rst = 1'b1;
+		#50;
+		traceClk <= 1'b1;
 		#1000;
 
 		rst = 1'b0;
+		#50;
+		traceClk <= 1'b0;
 		#1000;
 
-		setTrace(4'b0000);
-		setTrace(4'b0001);
-		setTrace(4'b0010);
+		`define TRACEWIDTH 1
 
-		// sync
-		setTrace(4'b0111);	// 7
-		setTrace(4'b1111);	// f
-		setTrace(4'b1111);	// f
-		setTrace(4'b1111);	// f
+		setTrace1(16'h0123);
+		setTrace1(16'h0123);
+		setTrace1(16'h0123);
+		setTrace1(16'h0123);
 
-		// frame 1
-		setTrace(4'b0000);	// byte 0
-		setTrace(4'b0001);	// byte 1
-		setTrace(4'b0010);	// byte 2
-		setTrace(4'b0011);	// byte 3
-		setTrace(4'b0100);	// byte 4
-		setTrace(4'b0101);	// byte 5
-		setTrace(4'b0110);	// byte 6
-		setTrace(4'b0111);	// byte 7
-		setTrace(4'b1000);	// byte 8
-		setTrace(4'b1001);	// byte 9
-		setTrace(4'b1010);	// byte 10
-		setTrace(4'b1011);	// byte 11
-		setTrace(4'b1100);	// byte 12
-		setTrace(4'b1101);	// byte 13
-		setTrace(4'b1110);	// byte 14
-		setTrace(4'b1111);	// byte 15
+		setTrace1(16'h7fff);
+		setTrace1(16'hffff);
+		setTrace1(16'hffff);
+		setTrace1(16'hffff);
 
-		// frame 2
-		setTrace(4'b0000);	// byte 0
-		setTrace(4'b0001);	// byte 1
-		setTrace(4'b0010);	// byte 2
-		setTrace(4'b0011);	// byte 3
-		setTrace(4'b0100);	// byte 4
-		setTrace(4'b0101);	// byte 5
-		setTrace(4'b0110);	// byte 6
-		setTrace(4'b0111);	// byte 7
-		setTrace(4'b1000);	// byte 8
-		setTrace(4'b1001);	// byte 9
-		setTrace(4'b1010);	// byte 10
-		setTrace(4'b1011);	// byte 11
-		setTrace(4'b1100);	// byte 12
-		setTrace(4'b1101);	// byte 13
-		setTrace(4'b1110);	// byte 14
-		setTrace(4'b1111);	// byte 15
+		setTrace1(16'h0123);
+		setTrace1(16'h4567);
+		setTrace1(16'h89ab);
+		setTrace1(16'hcdef);
+		setTrace1(16'h0123);
+		setTrace1(16'h4567);
+		setTrace1(16'h89ab);
+		setTrace1(16'hcdef);
 
-		// sync
-		setTrace(4'b0111);	// 7
-		setTrace(4'b1111);	// f
-		setTrace(4'b1111);	// f
-		setTrace(4'b1111);	// f
+		setTrace1(16'h7fff);
+		setTrace1(16'hffff);
+		setTrace1(16'hffff);
+		setTrace1(16'hffff);
 
-		// frame 3
-		setTrace(4'b0000);	// byte 0
-		setTrace(4'b0001);	// byte 1
-		setTrace(4'b0010);	// byte 2
-		setTrace(4'b0011);	// byte 3
-		setTrace(4'b0100);	// byte 4
-		setTrace(4'b0101);	// byte 5
-		setTrace(4'b0110);	// byte 6
-		setTrace(4'b0111);	// byte 7
-		setTrace(4'b1000);	// byte 8
-		setTrace(4'b1001);	// byte 9
-		setTrace(4'b1010);	// byte 10
-		setTrace(4'b1011);	// byte 11
-		setTrace(4'b1100);	// byte 12
-		setTrace(4'b1101);	// byte 13
-		setTrace(4'b1110);	// byte 14
-		setTrace(4'b1111);	// byte 15
+		setTrace1(16'h0123);
 
-		// frame 4 (broken)
-		setTrace(4'b0000);	// byte 0
-		setTrace(4'b0001);	// byte 1
-		setTrace(4'b0010);	// byte 2
-		setTrace(4'b0011);	// byte 3
-		setTrace(4'b0100);	// byte 4
-		setTrace(4'b0101);	// byte 5
-
-		// sync
-		setTrace(4'b0111);	// 7
-		setTrace(4'b1111);	// f
-		setTrace(4'b1111);	// f
-		setTrace(4'b1111);	// f
-
-		// frame 5 (inverted)
-		setTrace(4'b1111);	// byte 1
-		setTrace(4'b1110);	// byte 0
-		setTrace(4'b1101);	// byte 2
-		setTrace(4'b1100);	// byte 3
-		setTrace(4'b1011);	// byte 4
-		setTrace(4'b1010);	// byte 5
-		setTrace(4'b1001);	// byte 6
-		setTrace(4'b1000);	// byte 7
-		setTrace(4'b0111);	// byte 8
-		setTrace(4'b0110);	// byte 9
-		setTrace(4'b0101);	// byte 01
-		setTrace(4'b0100);	// byte 00
-		setTrace(4'b0011);	// byte 02
-		setTrace(4'b0010);	// byte 03
-		setTrace(4'b0001);	// byte 04
-		setTrace(4'b0000);	// byte 05
-
-		// frame 6
-		setTrace(4'b0000);	// byte 0
-		setTrace(4'b0001);	// byte 1
-		setTrace(4'b0010);	// byte 2
-		setTrace(4'b0011);	// byte 3
-		setTrace(4'b0100);	// byte 4
-		setTrace(4'b0101);	// byte 5
-		setTrace(4'b0110);	// byte 6
-		setTrace(4'b0111);	// byte 7
-		setTrace(4'b1000);	// byte 8
-		setTrace(4'b1001);	// byte 9
-		setTrace(4'b1010);	// byte 10
-		setTrace(4'b1011);	// byte 11
-		setTrace(4'b1100);	// byte 12
-		setTrace(4'b1101);	// byte 13
-		setTrace(4'b1110);	// byte 14
-		setTrace(4'b1111);	// byte 15
+		setTrace1(16'h7fff);
+		setTrace1(16'hffff);
+		setTrace1(16'hffff);
+		setTrace1(16'hffff);
 
 		#10000;
 		$display("simulation completed");
